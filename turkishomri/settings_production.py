a0a59cb755d74497ff01468bd561ccc946f158e4
azizbaba237 <abdoulazizbaba227@gmail.com>
2025-04-29 15:20:24 +0100

    Ajout des paramètres de production

---
+++ turkishomri/settings_production.py
@@ -0,0 +1,12 @@
+from .settings import *
+
+DEBUG = False
+ALLOWED_HOSTS = ['azizbaba237.pythonanywhere.com']
+
+# Chemins pour les fichiers statiques
+STATIC_URL = '/static/'
+STATIC_ROOT = '/home/Azizbaba237/turkisho/staticfiles'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/home/Azizbaba237/turkisho/media'
+
+# Ajoutez ici d'autres paramètres spécifiques à la production
